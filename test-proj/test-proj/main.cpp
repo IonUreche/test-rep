cccb66023eeb432dd5d22fdc6c536746599ef506
Ion Ureche <ureche.ion@gmail.com>
2021-04-11 15:44:06 +0300

    Huge Refactor and small adjustments to algorithm Gold #1250

--- test-proj/test-proj/main.cpp
+++ test-proj/test-proj/main.cpp
@@ -10,9 +10,10 @@ using namespace std;
 
 // ====== constants ============
 const int k_nrPods = 4;
-const int k_maxCheckpoints = 4;
+const int k_maxCheckpoints = 8;
 const double k_CheckpointRadius = 600;
 const int k_PodRadius = 400;
+const double k_frictionfactor = 0.85;
 
 // ====== helpers ============
 double clamp(double x)
@@ -36,12 +37,24 @@ double g_normDirVecY[k_maxCheckpoints];
 int g_optimalCheckpointId = -1;
 int g_nrLaps = 0;
 int g_nrCheckpoints = 0;
+int g_turn = -1;
+int g_currentLap = 0;
 
 int SquaredIntDistance(int x0, int y0, int x1, int y1)
 {
 	int dx = (x0 - x1);
 	int dy = (y0 - y1);
 	return dx * dx + dy * dy;
+}
+
+double vec_dot(int x0, int y0, int x1, int y1)
+{
+	double l1 = sqrt(x0 * x0 + y0 * y0);
+	double l2 = sqrt(x1 * x1 + y1 * y1);
+	double cos = (x0 * x1 + y0 * y1);
+	cos /= l1;
+	cos /= l2;
+	return abs(cos);
 }
 
 void ComputeOptimalBoostCheckpointId()
@@ -99,74 +112,171 @@ struct Pod
 
 	void WriteTurnOutput(std::ostream& out)
 	{
-		out << g_checkPointPosX[nextCheckPointId] << ' ' << g_checkPointPosY[nextCheckPointId] << ' ';
-		//if(enableShield && (turn - turnShieldEnabled) > 3)
-		//{
-		//    out << "SHIELD" << endl;
-		//    turnShieldEnabled = turn;
-		//}
-		//else
-		//if(!boostUsed && boost)
-		//{
-		//    out << "BOOST" << endl;
-		//    boost = false;
-		//} 
-		//else
+		// in order to try to avoid curve drifting, we can send modified NextCheckPoint coordinates
+		// we want it to be speed dependent, because on high speed we will drift anyway, while on slow speed we dont want to change coordinate too much
+		out << GetAdjustedCheckpoint(0) << ' ' << GetAdjustedCheckpoint(1) << ' ';
+		if (shieldEnabled && (g_turn - turnShieldWasEnabled) > 3)
 		{
-			out << thrust << endl;
+			out << "SHIELD" << endl;
+			turnShieldWasEnabled = g_turn;
+			shieldEnabled = false;
 		}
+		else
+			if (useBoost)
+			{
+				out << "BOOST" << endl;
+				useBoost = false;
+				boostUsed = true;
+			}
+			else
+			{
+				out << thrust << endl;
+			}
+	}
+
+	void Update()
+	{
+		UpdateCurrentLap();
+		UpdateAngleFactor();
+		UpdateThrust();
+		TryUseBoost();
+	}
+
+	void UpdateCurrentLap()
+	{
+		if (previousCheckPointId == g_nrCheckpoints - 1 && nextCheckPointId == 0)
+		{
+			++currentLap;
+		}
+		previousCheckPointId = nextCheckPointId;
+	}
+
+	void UpdateAngleFactor()
+	{
+		if (g_turn == 0)
+		{
+			angleFactor = 1.0;
+			return;
+		}
+		if (angle > 180) angle = angle - 360;
+		//cerr << angle << " ";
+		int nextChpX = g_checkPointPosX[nextCheckPointId] - x;
+		int nextChpY = g_checkPointPosY[nextCheckPointId] - y;
+		double dirVecLen = sqrt(nextChpX * nextChpX + nextChpY * nextChpY);
+		double normProjX = nextChpX / dirVecLen;
+		double arc = acos(normProjX);
+		if (nextChpY < 0.0) arc = -arc;
+		//cerr << arc << " ";
+		nextCheckpointAngle = (arc * 180.0 / PI) - angle;
+		if (nextCheckpointAngle <= 30 && nextCheckpointAngle >= -30)
+		{
+			angleFactor = 1.0;
+		}
+		else
+			if (nextCheckpointAngle <= 90 && nextCheckpointAngle >= -90)
+			{
+				angleFactor = 1.0 - ((abs(nextCheckpointAngle) - 30.0) / 60.0);
+			}
+			else
+			{
+				angleFactor = 0.0;
+			}
+
+		// slow down before reaching a checkpoint in order to avoid big curves
+		// the radius of the checkpoint multiplied by a function cosine of angle between the target and the next checkpoint should be okay to start slowing down
+		double slowDownStartDistance = GetSlowDownFactor() * k_CheckpointRadius;
+		int nextCheckpointDist = sqrt(SquaredIntDistance(x, y, g_checkPointPosX[nextCheckPointId], g_checkPointPosY[nextCheckPointId]));
+		double slowDownFactor = clamp(nextCheckpointDist / slowDownStartDistance);
+
+		angleFactor *= slowDownFactor;
+		//cerr << slowDownStartDistance << " __ " << slowDownFactor << '\n';
+		//cerr << nextCheckpointAngle << " $ \n";
 	}
 
 	void UpdateThrust()
 	{
-		angleFactor = 1.0;
 		thrust = 100 * angleFactor;
+	}
+
+	void PredictCollision(const Pod pods[k_nrPods])
+	{
+		// Collision prediction for the next 2 frames
+		// also checking if it's optimal to shield a collision based on the dot product between speed vectors of the colliding pods
+
+		for (int i = 2; i < 4; ++i) // traverse only the enemy pods
+		{
+			// compute the distance between the centers of the two pods
+			int x0 = x + (i - 1) * vx * k_frictionfactor;
+			int y0 = y + (i - 1) * vy * k_frictionfactor;
+			int x1 = pods[i].x + (i - 1) * pods[i].vx * k_frictionfactor;
+			int y1 = pods[i].y + (i - 1) * pods[i].vy * k_frictionfactor;
+			int dx = x1 - x0;
+			int dy = y1 - y0;
+			int distSq = dx * dx + dy * dy;
+			//cerr << distSq << " ... " << 4 * k_PodRadius * k_PodRadius << endl;
+			double cosVal = vec_dot(vx, vy, pods[i].vx, pods[i].vy);
+			//cerr << cosVal << ' ';
+			if (dx * dx + dy * dy <= 4 * k_PodRadius * k_PodRadius && cosVal > 0.8)
+				// if cosine of the velocities are > 0.8 we want to shield to prevent deviation in a wrong direction
+			{
+				shieldEnabled = true;
+			}
+		}
+		//cerr << " #\n";
+	}
+
+	int GetAdjustedCheckpoint(int coord)
+	{
+		const int distScale = 3;
+		if (coord == 0) return g_checkPointPosX[nextCheckPointId] + distScale * (g_normDirVecX[nextCheckPointId] - vx);
+		return g_checkPointPosY[nextCheckPointId] + distScale * (g_normDirVecY[nextCheckPointId] - vy);
 	}
 
 	double GetSlowDownFactor()
 	{
-		//int idx = (m_currentCheckPointIndex + m_arraySize - 1) % m_arraySize;
-		return 0.1 + 2 * g_angles[nextCheckPointId];
+		//cerr << g_angles[nextCheckPointId] << " # ";
+		return 0.1 + 2.9 * (1 - g_angles[nextCheckPointId]);
 	}
 
-	bool IsOptimalToUseBoost(int nextX, int nextY)
+	void TryUseBoost()
 	{
-		return nextCheckPointId == g_optimalCheckpointId;
+		if (!boostUsed && currentLap > 0 && nextCheckPointId == g_optimalCheckpointId
+			&& nextCheckpointAngle >= -10 && nextCheckpointAngle <= 10) // we want pod to be aligned in order to boost when moving in almost straight line
+		{
+			useBoost = true;
+		}
 	}
 
 	int x, y;
 	int vx, vy;
 	int angle;
 	int nextCheckPointId;
+	int nextCheckpointAngle;
 
 	bool shieldEnabled = false;
 	bool boostUsed = false;
+	bool useBoost = false;
 	int thrust;
-
+	int index;
+	int turnShieldWasEnabled = -1;
 	double angleFactor;
+	int previousCheckPointId = -1;
+	int currentLap = 0;
 };
 
 int main()
 {
 	bool usedBoost = false;
-	const int thresholdDistForFirstBoost = 1000;
-
-	int thrust;
 	bool boost = false;
-	double angleFactor;
 
 	Pod pods[k_nrPods];
+	for (int i = 0; i < 4; ++i)
+		pods[i].index = i;
 
-	int turnShieldEnabled = -5;
-	int turn = -1;
-
-	//int Nrlaps, nrCkp, c_x, c_y;
 	cin >> g_nrLaps >> g_nrCheckpoints;
-	//ckManager.InitCheckpoints(Nrlaps, nrCkp);
 	for (int i = 0; i < g_nrCheckpoints; ++i)
 	{
 		cin >> g_checkPointPosX[i] >> g_checkPointPosY[i];
-		//ckManager.AddCheckpoint(g_checkPointPosX[i], g_checkPointPosY[i]);
 	}
 
 	ComputeOptimalBoostCheckpointId();
@@ -174,116 +284,17 @@ int main()
 
 	// game loop
 	while (1) {
-		++turn;
-		//cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
-		//cin >> opponentX >> opponentY; cin.ignore();
+		++g_turn;
 		for (int i = 0; i < k_nrPods; ++i)
 		{
 			pods[i].ReadTurnInput(cin);
 		}
 
-		//double cosine = cos ( nextCheckpointAngle * PI / 180.0 );
-		//// slow down before reaching a checkpoint in order to avoid big curves
-		//
-		//double slowDownStartDistance = (ckManager.GetSlowDownFactor()) * k_CheckpointRadius; // twice the radius of the checkpoint should be okay to start slowing down
-		//double slowDownFactor = clamp(nextCheckpointDist / slowDownStartDistance);
-
-		//if(nextCheckpointAngle <= 30 && nextCheckpointAngle >= -30)
-		//{
-		//    angleFactor = 1.0;
-		//}
-		//else
-		//if(nextCheckpointAngle <= 90 && nextCheckpointAngle >= -90)
-		//{
-		//    angleFactor = 1.0 - ((abs(nextCheckpointAngle) - 30.0) / 60.0);
-		//}
-		//else
-		//{
-		//    angleFactor = 0.0;
-		//}
-//
-		//angleFactor *= slowDownFactor;
-
-		// try to use boost
-		// we want to boost when the angle between pod orientation and the next checkpoint is as small as possible
-		//double thresholdAngleFactor = 0.95; // plus epsilon just in case
-		//double angle = acos(ckManager.GetSlowDownFactor()) * 180.0 / PI;
-		////cerr << angle << " " << slowDownStartDistance << " " << ckManager.GetSlowDownFactor() << " " << usedBoost << " " << ckManager.m_cycleFinished << " " << ckManager.m_optimCheckpointX << " " << ckManager.m_optimCheckpointY << " " << cosine  << " " << nextCheckpointDist << " " << angleFactor << endl;
-		//if(!usedBoost && cosine >= thresholdAngleFactor && ckManager.IsOptimalToUseBoost(nextCheckpointX, nextCheckpointY))
-		//{
-		//    boost = true;
-		//    usedBoost = true;
-		//}
-		//else
-		//{
-		//    thrust = static_cast<int>(100 * angleFactor);
-		//}
-
-		// You have to output the target position
-		// followed by the power (0 <= thrust <= 100)
-		// i.e.: "x y thrust"
-
-		// in order to try to avoid curve drifting, we can send modified NextCheckPoint coordinates
-		// we want it to be speed dependent, because on high speed we will drift anyway, while on slow speed we dont want to change coordinate too much
-
-		// speed is the derivative of the position so we can take the delta pos between current and last frame
-		//int deltaSpeedX = x - prevX;
-		//int deltaSpeedY = y - prevY;
-		//int distScale = 4;
-//
-		//prevX = x;
-		//prevY = y;
-//
-		//int offX = nextCheckpointX - deltaSpeedX * distScale + distScale * ckManager.GetCurrentNormDirX();
-		//int offY = nextCheckpointY - deltaSpeedY * distScale + distScale * ckManager.GetCurrentNormDirY();
-
-		//cerr << nextCheckpointX << " " << nextCheckpointY << " " ;
-		//cout << offX << " " << offY << " ";
-
-		// Collision prediction for the next 2 frames
-		// for now just naive shielding before collision
-		// next upgrade would be to determine when it makes sense to shield and when it's okay to take extra velocity from collision
-		//int enemyDeltaSpeedX = opponentX - enemyPrevX;
-		//int enemyDeltaSpeedY = opponentY - enemyPrevY;
-		//enemyPrevX = opponentX;
-		//enemyPrevY = opponentY;
-		//bool enableShield = false;
-		//for(int i = 0; i < 2; ++i)
-		//{
-		//    int x0 = x + (i+1) * deltaSpeedX;
-		//    int y0 = y + (i+1) * deltaSpeedY;
-		//    int x1 = opponentX + (i+1) * enemyDeltaSpeedX;
-		//    int y1 = opponentY + (i+1) * enemyDeltaSpeedY;
-		//    int dx = x1 - x0;
-		//    int dy = y1 - y0;
-		//    int distSq = dx * dx + dy * dy;
-		//    cerr << distSq << " ... " << 4 * k_PodRadius * k_PodRadius << endl;
-		//    if(dx * dx + dy * dy <= 4 * k_PodRadius * k_PodRadius)
-		//    {
-		//        enableShield = true;
-		//    }
-		//}
-
 		for (int i = 0; i < 2; ++i)
 		{
-			pods[i].UpdateThrust();
+			pods[i].Update();
+			pods[i].PredictCollision(pods);
 			pods[i].WriteTurnOutput(cout);
 		}
-
-		//if(enableShield && (turn - turnShieldEnabled) > 3)
-		//{
-		//    cout << "SHIELD" << endl;
-		//    turnShieldEnabled = turn;
-		//}
-		//else
-		//if(boost)
-		//{
-		//    cout << "BOOST" << endl;
-		//    boost = false;
-		//} 
-		//else
-		//{
-		//    cout << thrust << endl;
-		//}
 	}
 }
